a466a0520a194f1be49aca325d483792bdf530b7
Krzysztof Hadaś <krzysztofhadas6@gmail.com>
2015-07-18 20:36:39 +0200

    Added very simple angularjs app which consume rest service

---
+++ rest-consumer/src/main/java/app/SampleWebStaticApplication.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SampleWebStaticApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleWebStaticApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleWebStaticApplication.class, args);
+    }
+
+}
